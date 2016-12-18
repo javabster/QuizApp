b138522a7c298edcf6b1025f15c42d3866a0a60f
Abby Mitchell <abby.mitchell.15@ucl.ac.uk>
2016-12-18 21:53:42 +0000

    made a fragment, Random Questions still not working but have emailed prof Harbrid who says she'll take a look and see if she can see whats wrong

--- app/src/main/java/am/app/MainActivity.java
+++ app/src/main/java/am/app/MainActivity.java
@@ -1,11 +1,16 @@
 package am.app;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+
+
+
+public class MainActivity extends FragmentActivity implements BlankFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,4 +31,8 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
